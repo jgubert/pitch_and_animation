b65c7c6f2dbb63e76e39f4b406a3979092f8d932
João Gubert <joao-gubert@hotmail.com>
2020-11-05 23:51:08 -0300

    Adicionando marcador de tempo

--- pitch_and_animation.py
+++ pitch_and_animation.py
@@ -16,7 +16,7 @@ class Game():
         self.time = 30
 
 
-sr, audio = wavfile.read('sounds\parabens.wav')
+sr, audio = wavfile.read('sounds\Beethoven_Piano_Sonata_21,_1st_movement,_bars_78-84.wav')
 time, frequency, confidence, activation = crepe.predict(audio, sr, viterbi=True)
 
 
@@ -31,11 +31,14 @@ menu_font = pygame.font.SysFont(font_name, 36)
 
 screen = pygame.display.set_mode((800,600), 0, 32)
 
-background_filename = 'images\\background.png'
+background_filename = 'images\\background2.png'
 background = pygame.image.load(background_filename).convert()
 
-track1_filename = 'images\\track1.png'
+track1_filename = 'images\\track2.png'
 track1 = pygame.image.load(track1_filename).convert()
+
+marker_filename = 'images\\s_marker.png'
+marker1 = pygame.image.load(marker_filename).convert()
 
 pygame.display.set_caption('Pitch and Animation')
 clock = pygame.time.Clock()
@@ -64,6 +67,19 @@ class Track(Object):
         elif self.way == "left":
             self.position[0] = self.position[0] - speed * self.factor
 
+class Marker(Object):
+    def __init__(self,position,sprite_enemy,way,factor):
+        self.sprite = marker1
+        self.position = position
+        self.way = way
+        self.factor = factor
+
+    def move(self,speed):
+        if self.way == "right":
+            self.position[0] = self.position[0] + speed * self.factor
+        elif self.way == "left":
+            self.position[0] = self.position[0] - speed * self.factor
+
 #Funções gerais
 def drawList(list):
     for i in list:
@@ -75,17 +91,21 @@ def moveList(list,speed):
 
 def destroyTracks(list):
     for i in list:
-        if i.position[0] < -10:
-            list.remove(i)
-        elif i.position[0] > 800:
+        if i.position[0] < 60:
             list.remove(i)
 
 def createTrack(tracks, frequency):
-    position_init = [450, frequency]
+    position_init = [700, frequency]
     track = Track(position_init, track1,"left", 1)
     tracks.append(track)
 
-trilha_sound = pygame.mixer.Sound('sounds\parabens.wav')
+def createMarker(markers):
+    position_init = [700, 50]
+    #position_init = [300, 400]
+    marker = Marker(position_init, marker1, "left", 1)
+    markers.append(marker)
+
+trilha_sound = pygame.mixer.Sound('sounds\Beethoven_Piano_Sonata_21,_1st_movement,_bars_78-84.wav')
 
 screen.blit(background, (0, 0))
 text_info = menu_font.render(('Press any button to start!'), 1,(255,255,255))
@@ -104,6 +124,7 @@ while gameInit == 0:
     pygame.display.update()
 
 tracks = []
+marker = []
 time_counter = 0
 ticks_time = 30
 trilha_sound.play(1)
@@ -118,22 +139,29 @@ while True:
     if not ticks_time:
         ticks_time = 100
         time_counter = time_counter + 1
-        
+        createMarker(marker)
         
     else:
         ticks_time = ticks_time - 1
 
     if len(frequency) != 0:
-        createTrack(tracks, int(frequency[0]))
+        createTrack(tracks, int(600 - frequency[0]/8))
         frequency = np.delete(frequency, 0)
+    
+    # Acabou o pitch tracking
+    else:
+        trilha_sound.stop()
             
     screen.blit(background, (0, 0))
     moveList(tracks, 3)
+    moveList(marker, 3)
     drawList(tracks)
+    drawList(marker)
     text_info2 = info_font.render(('Time: {0}'.format(time_counter,)),1,(255,255,255))
-    screen.blit(text_info2,(150,50))
+    screen.blit(text_info2,(60,10))
     
-    #destroyTracks(tracks)
+    destroyTracks(tracks)
+    destroyTracks(marker)
 
     pygame.display.update()
     time_passed = clock.tick(100)
