63f68ceb95ced8881e806f9351db96f09094a633
João Gubert <joao-gubert@hotmail.com>
2020-11-09 00:19:15 -0300

    Adicionando escala

--- pitch_and_animation.py
+++ pitch_and_animation.py
@@ -4,6 +4,8 @@ import pygame
 from pygame.locals import *
 import numpy as np
 
+str_sound = 'Beethoven_-_Diabelli_Variations_-_00.wav'
+
 class Game():
     def __init__(self):
         self.time = 30
@@ -15,9 +17,49 @@ class Game():
     def resetTime(self):
         self.time = 30
 
+#text_info = menu_font.render(('Press any button to start!'), 1,(255,255,255))
 
-sr, audio = wavfile.read('sounds\Beethoven_Piano_Sonata_21,_1st_movement,_bars_78-84.wav')
-time, frequency, confidence, activation = crepe.predict(audio, sr, viterbi=True)
+def getMaxFreq(freq):
+    division = 1
+    str_1 = ''
+    str_2 = ''
+    str_3 = ''
+    str_4 = ''
+    str_5 = ''
+    max_freq = frequency.max()
+    if max_freq <= 500:
+        division = 1
+        max_freq_s = 500
+    elif max_freq <= 1000:
+        division = 2
+        max_freq_s = 1000
+    elif max_freq <= 1500:
+        division = 3
+        max_freq_s = 1500
+    elif max_freq <= 2000:
+        division = 4
+        max_freq_s = 2000
+    elif max_freq <= 2500:
+        division = 5
+        max_freq_s = 2500
+    elif max_freq <= 3000:
+        division = 6
+        max_freq_s = 3000
+    elif max_freq <= 3500:
+        division = 7
+        max_freq_s = 3500
+    else:
+        division = 8
+        max_freq_s = 4000
+    
+    str_1 = str(max_freq_s) + 'Hz'
+    str_2 = str(int(max_freq_s*0.8)) + 'Hz'
+    str_3 = str(int(max_freq_s*0.6)) + 'Hz'
+    str_4 = str(int(max_freq_s*0.4)) + 'Hz'
+    str_5 = str(int(max_freq_s*0.2)) + 'Hz'
+
+    return division, str_1, str_2, str_3, str_4, str_5
+
 
 
 pygame.init()
@@ -42,6 +84,7 @@ marker1 = pygame.image.load(marker_filename).convert()
 
 pygame.display.set_caption('Pitch and Animation')
 clock = pygame.time.Clock()
+
 
 class Object():
     def __init__(self,position,sprite):
@@ -105,12 +148,33 @@ def createMarker(markers):
     marker = Marker(position_init, marker1, "left", 1)
     markers.append(marker)
 
-trilha_sound = pygame.mixer.Sound('sounds\Beethoven_Piano_Sonata_21,_1st_movement,_bars_78-84.wav')
+trilha_sound = pygame.mixer.Sound('sounds/' + str_sound)
 
 screen.blit(background, (0, 0))
-text_info = menu_font.render(('Press any button to start!'), 1,(255,255,255))
+text_music = info_font.render(('Musica: ' + str_sound), 1,(255,255,255))
+text_info = info_font.render(('Realizando pitch tracking...'), 1,(255,255,255))
 gameInit = 0
 
+screen.blit(background, (0, 0))
+screen.blit(text_info,(65,30))
+screen.blit(text_music,(65,10))
+pygame.display.update()
+
+sr, audio = wavfile.read('sounds/' + str_sound)
+time, frequency, confidence, activation = crepe.predict(audio, sr, viterbi=True)
+division, str1, str2, str3, str4, str5 = getMaxFreq(frequency)
+
+text_division1 = info_font.render((str1), 1,(255,255,255))
+text_division2 = info_font.render((str2), 1,(255,255,255))
+text_division3 = info_font.render((str3), 1,(255,255,255))
+text_division4 = info_font.render((str4), 1,(255,255,255))
+text_division5 = info_font.render((str5), 1,(255,255,255))
+
+screen.blit(background, (0, 0))
+text_info = info_font.render(('Pressione qualquer tecla para continuar'), 1,(255,255,255))
+screen.blit(text_info,(65,30))
+screen.blit(text_music,(65,10))
+pygame.display.update()
 
 while gameInit == 0:
     for event in pygame.event.get():
@@ -120,7 +184,7 @@ while gameInit == 0:
             gameInit = 1
 
     screen.blit(background, (0, 0))
-    screen.blit(text_info,(300,400))
+    screen.blit(text_info,(65,30))
     pygame.display.update()
 
 tracks = []
@@ -145,7 +209,7 @@ while True:
         ticks_time = ticks_time - 1
 
     if len(frequency) != 0:
-        createTrack(tracks, int(600 - frequency[0]/8))
+        createTrack(tracks, int(600 - frequency[0]/division))
         frequency = np.delete(frequency, 0)
     
     # Acabou o pitch tracking
@@ -157,8 +221,14 @@ while True:
     moveList(marker, 3)
     drawList(tracks)
     drawList(marker)
-    text_info2 = info_font.render(('Time: {0}'.format(time_counter,)),1,(255,255,255))
-    screen.blit(text_info2,(60,10))
+    text_info2 = info_font.render(('Tempo: {0}'.format(time_counter,)),1,(255,255,255))
+    screen.blit(text_music,(65,10))
+    screen.blit(text_info2,(65,30))
+    screen.blit(text_division1,(10,85))
+    screen.blit(text_division2,(10,185))
+    screen.blit(text_division3,(10,285))
+    screen.blit(text_division4,(10,385))
+    screen.blit(text_division5,(10,485))
     
     destroyTracks(tracks)
     destroyTracks(marker)
